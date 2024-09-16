368c7e9b9ec982bf067ba83d6cf20d8d77e838f2
ha <hnngcs2@gmail.com>
2024-09-16 11:38:37 +0700

    complete edit and details action for Account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -77,7 +77,7 @@ namespace MedicineStock.Controllers
                 var usrName = _context.Accounts.Where(q => q.UserName == account.UserName).FirstOrDefault();
                 if (usrName != null)
                 {
-                    ViewData["MedicineTypeId"] = new SelectList(_context.Permissions, "PermissionId", "Name", account.PermissionId);
+                    ViewData["PermissionId"] = new SelectList(_context.Permissions, "PermissionId", "Name", account.PermissionId);
                     return View(account);
                 }
                 _context.Add(account);
@@ -85,8 +85,87 @@ namespace MedicineStock.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewData["MedicineTypeId"] = new SelectList(_context.Permissions, "PermissionId", "Name", account.PermissionId);
+            ViewData["PermissionId"] = new SelectList(_context.Permissions, "PermissionId", "Name", account.PermissionId);
             return View(account);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            ViewData["Permissions"] = await _context.Permissions.ToListAsync();
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Medicines/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["PermissionId"] = new SelectList(_context.Permissions, "PermissionId", "Name", account.PermissionId);
+            return View(account);
+        }
+
+        // POST: Medicines/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AccountId","UserName,Password,PermissionId")] Account account)
+        {
+            if (id != account.AccountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.AccountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["PermissionId"] = new SelectList(_context.Permissions, "PermissionId", "Name", account.PermissionId);
+            return View(account);
+            //return await Index();
+        }
+
+        private bool AccountExists(int id)
+        {
+            return _context.Medicines.Any(e => e.MedicineId == id);
         }
     }
 }
